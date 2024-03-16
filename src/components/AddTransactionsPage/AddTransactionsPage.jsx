4f8afa02a22515997b1b44e21edf3c4a67005034
Gabriel Regalado <regs0404@gmail.com>
2024-03-16 16:52:09 -0400

    added in html markup for add transaction form

--- src/components/AddTransactionsPage/AddTransactionsPage.jsx
+++ src/components/AddTransactionsPage/AddTransactionsPage.jsx
@@ -1,5 +1,5 @@
 import './AddTransactionsPage';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 function AddTransactionsPage() {
@@ -25,16 +25,25 @@ function AddTransactionsPage() {
       <div>
         <h1>Add Transactions</h1>
       </div>
-      <div className="category">
-        <label htmlFor="category">Select Category:</label>
-        <select id="category" onChange={handleCategorySelect}>
-          <option value="">Select a category</option>
-          {category.map((categoryName) => (
-            <option key={categoryName.id} value={categoryName.id}>
-              {categoryName.name}
-            </option>
-          ))}
-        </select>
+      <div className="transactionForm">
+        <form>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name"/>
+          <label htmlFor="amount">Amount:</label>
+          <input type="number" id="amount"/>
+          <label htmlFor="category">Select Category:</label>
+          <select id="category" onChange={handleCategorySelect}>
+            <option value="">Select a category</option>
+            {category.map((categoryName) => (
+              <option key={categoryName.id} value={categoryName.id}>
+                {categoryName.name}
+              </option>
+            ))}
+          </select>
+          <label htmlFor="trans_date">Date:</label>
+          <input type="date" id="trans_date"/>
+          <button type="submit">Add Transaction</button>
+        </form>
       </div>
     </>
   );
