58bf850c65da9597d92ffeb1ba7f8fc77dc3f61a
Gabriel Regalado <regs0404@gmail.com>
2024-03-17 20:05:31 -0400

    added in a delete and put route for my expenses

--- server/routes/expenses.router.js
+++ server/routes/expenses.router.js
@@ -40,4 +40,46 @@ router.post('/', (req, res) => {
     });
 });
 
+/**
+ * DELETE route
+ */
+router.delete('/:id', (req, res) => {
+  const expenseId = Number(req.params.id);
+  const queryText = 'DELETE FROM "expenses" WHERE "id" = $1;';
+  pool
+    .query(queryText, [expenseId])
+    .then((results) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.error('Error deleting transaction:', error);
+      res.sendStatus(500);
+    });
+});
+
+/**
+ * PUT route
+ */
+router.put('/:id', (req, res) => {
+  const expenseId = req.params.id;
+  const { name, amount, category_id } = req.body;
+
+  const queryText = `
+    UPDATE "expenses" 
+    SET "amount" = $1
+    WHERE "id" = $2
+  `;
+  const queryValues = [amount, expenseId];
+
+  pool
+    .query(queryText, queryValues)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.error('Error updating expenses:', error);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = router;
