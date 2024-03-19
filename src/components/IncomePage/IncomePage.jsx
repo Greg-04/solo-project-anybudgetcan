f04179cd2684f8ac36f04b3c7a4413a6e3982b40
Gabriel Regalado <regs0404@gmail.com>
2024-03-19 19:39:42 -0400

    added handle for submission

--- src/components/IncomePage/IncomePage.jsx
+++ src/components/IncomePage/IncomePage.jsx
@@ -2,13 +2,32 @@ import './IncomePage.css';
 import React, { useState } from 'react';
 
 function IncomePage() {
-  //Setting up state for salary
+  //Setting up state for salary and income frequency
   const [salary, setSalary] = useState(0);
+  const [incomeFrequency, setIncomeFrequency] = useState('');
 
   //handle for salary change
   const handleSalaryChange = (event) => {
-    setSalary(Number(event.target.value));
-  }
+    //have to set values to first because of useState
+    const newSalary = event.target.value;
+    setSalary(newSalary);
+    // console.log('salary value:', newSalary);
+  };
+
+  //handle for frequency change
+  const handleFrequencyChange = (event) => {
+    //have to set values to first because of useState
+    const newFrequency = event.target.value;
+    setIncomeFrequency(newFrequency);
+    // console.log('income frequency:', newFrequency);
+  };
+
+  //handle for my income submit
+  const handleSubmit = (event) => {
+    alert('Income Submitted!');
+    setSalary('');
+    setIncomeFrequency('');
+  };
 
   return (
     <>
@@ -18,16 +37,16 @@ function IncomePage() {
         </div>
       </div>
       <main>
-        <form className="salaryForm">
+        <form className="salaryForm" onSubmit={handleSubmit}>
           <h2>Set your income</h2>
           <h1>${salary}</h1>
           <div>
             <p>Annual or Monthly?</p>
-            <select 
-            required 
-            id="income" 
-            value={null}
-            >
+            <select
+              required
+              id="income"
+              value={incomeFrequency}
+              onChange={handleFrequencyChange}>
               <option value="">Select</option>
               <option>Yearly</option>
               <option>Monthly</option>
@@ -35,12 +54,11 @@ function IncomePage() {
           </div>
           <div>
             <p>Enter your income:</p>
-            <input 
-            type="number"
-            value={salary}
-            onChange={handleSalaryChange}
-            required
-            ></input>
+            <input
+              type="number"
+              value={salary}
+              onChange={handleSalaryChange}
+              required></input>
           </div>
           <div>
             <br />
