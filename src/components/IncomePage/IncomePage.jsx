8e615dc8141eda0105cbf6f56bc6e2d58d194094
Gabriel Regalado <regs0404@gmail.com>
2024-03-19 19:08:17 -0400

    added html markup for income page

--- src/components/IncomePage/IncomePage.jsx
+++ src/components/IncomePage/IncomePage.jsx
@@ -1,19 +1,40 @@
 import './IncomePage.css';
-import React from 'react';
-
-// This is one of our simplest components
-// It doesn't have local state,
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
+import React, { useState } from 'react';
 
 function IncomePage() {
+  //Setting up state for salary
+  const [salary, setSalary] = useState(`$${Number(0)}`);
+
   return (
     <>
-    <div className="container">
-      <div>
-        <h1>Income Page</h1>
+      <div className="container">
+        <div>
+          <h1>Income Page</h1>
+        </div>
       </div>
-    </div>
+      <main>
+        <form>
+          <h2>Set your income</h2>
+          <h1>{salary}</h1>
+          <div>
+            <p>Annual or Monthly?</p>
+            <select required id="income" value={null}>
+              <option value="">Select</option>
+              <option>Yearly</option>
+              <option>Monthly</option>
+            </select>
+          </div>
+          <div>
+            <input type="number"></input>
+          </div>
+          <div>
+            <br />
+            <button>Submit</button>
+            <br />
+            <button>Next</button>
+          </div>
+        </form>
+      </main>
     </>
   );
 }
