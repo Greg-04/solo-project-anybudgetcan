e4bfef7314f895d45650cb297114af888e3f298b
Gabriel Regalado <regs0404@gmail.com>
2024-03-19 21:21:25 -0400

    set up state for each input and console logged

--- src/components/CreatePlanPage/CreatePlanPage.jsx
+++ src/components/CreatePlanPage/CreatePlanPage.jsx
@@ -1,7 +1,35 @@
 import './CreatePlanPage.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function CreatePlanPage() {
+  const [targetDate, setTargetDate] = useState('');
+  const [planName, setPlanName] = useState('');
+  const [targetAmount, setTargetAmount] = useState('');
+
+  //handle for name change
+  const handlePlanName = (event) => {
+    //have to set values to first because of useState
+    const planName = event.target.value;
+    setPlanName(planName);
+    // console.log('planName:', planName);
+  };
+
+  //handle for target date
+  const handleTargetDate = (event) => {
+    //have to set values to first because of useState
+    const targetDate = event.target.value;
+    setTargetDate(targetDate);
+    // console.log('targetDate:', targetDate);
+  };
+
+  //handle for target amount
+  const handleTargetAmount = (event) => {
+    //have to set values to first because of useState
+    const targetAmount = event.target.value;
+    setTargetAmount(targetAmount);
+    // console.log('targetAmount:', targetAmount);
+  };
+
   return (
     <>
       <div className="container">
@@ -15,15 +43,27 @@ function CreatePlanPage() {
 
           <div>
             <p>Set Target Date:</p>
-            <input type="date" required></input>
+            <input
+              type="date"
+              value={targetDate}
+              onChange={handleTargetDate}
+              required></input>
           </div>
           <div>
             <p>Plan Name:</p>
-            <input type="text" required></input>
+            <input
+              type="text"
+              value={planName}
+              onChange={handlePlanName}
+              required></input>
           </div>
           <div>
             <p>Set Target Amount</p>
-            <input type="number"></input>
+            <input
+              type="number"
+              value={targetAmount}
+              onChange={handleTargetAmount}
+              required></input>
           </div>
           <div>
             <br />
