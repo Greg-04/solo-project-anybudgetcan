e2b00d7d4c99bee4054a80546339422bc9af41e3
Gabriel Regalado <regs0404@gmail.com>
2024-03-20 21:21:39 -0400

    installed chartjs

--- src/components/BudgetOverviewPage/BudgetOverviewPage.jsx
+++ src/components/BudgetOverviewPage/BudgetOverviewPage.jsx
@@ -1,5 +1,6 @@
 import './BudgetOverviewPage.css';
 import React from 'react';
+import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js';
 
 function BudgetOverviewPage() {
   return (
