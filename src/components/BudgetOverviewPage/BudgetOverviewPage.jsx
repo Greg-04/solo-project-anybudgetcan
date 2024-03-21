dae368a2e4dd2b807229aabe1bd15803093997d3
Gabriel Regalado <regs0404@gmail.com>
2024-03-20 21:31:28 -0400

    set up first pie chart

--- src/components/BudgetOverviewPage/BudgetOverviewPage.jsx
+++ src/components/BudgetOverviewPage/BudgetOverviewPage.jsx
@@ -1,8 +1,20 @@
 import './BudgetOverviewPage.css';
 import React from 'react';
-import {Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+ChartJS.register(Tooltip, Legend, ArcElement);
 
 function BudgetOverviewPage() {
+  const data = {
+    labels: ['one', 'two', 'three'],
+    datasets: [
+      { data: [3, 6, 9], backgroundColor: ['aqua', 'orangered', 'purple'] },
+    ],
+  };
+
+  const options = {};
+
   return (
     <>
       <div className="container">
@@ -10,6 +22,18 @@ function BudgetOverviewPage() {
           <h1>Budget Overview</h1>
         </div>
       </div>
+      <main>
+        <div
+          className="expenseChart"
+          style={{
+            padding: '20px',
+            width: '50%',
+          }}>
+          <Pie data={data} option={options}></Pie>
+        </div>
+        <div className="transactionChart"></div>
+        <div className="combinedChart"></div>
+      </main>
     </>
   );
 }
