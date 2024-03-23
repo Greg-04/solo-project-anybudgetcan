6c29145333bad9e21717455b601e8a1300aad0b3
Gabriel Regalado <regs0404@gmail.com>
2024-03-23 16:42:22 -0400

    created theme provider in index.jsx file

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,31 @@ import store from './redux/store';
 
 import App from './components/App/App';
 
+//material ui custom theming
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
+
+//theme options are on the configuration object
+const themeConfig = {
+  palette: {
+    primary: {
+      main: grey[300],
+    },
+    secondary: {
+      main: '#111111',
+    },
+  },
+};
+
+const newTheme = createTheme(themeConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('react-root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={newTheme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
