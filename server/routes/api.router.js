aca6793a332a0716cc9e3a7287683fe6f89d7477
Gabriel Regalado <regs0404@gmail.com>
2024-04-09 18:27:17 -0400

    working on api router

--- server/routes/api.router.js
+++ server/routes/api.router.js
@@ -4,7 +4,31 @@ const router = express.Router();
 const axios = require('axios');
 
 const API_KEY = process.env.API_KEY;
-// console.log('key:', API_KEY);
+// console.log('API key:', API_KEY);
+
+//from docuclipper documentation
+const FormData = require('form-data');
+const fs = require('fs');
+const jwtToken = API_KEY;
+const filePath = '';
+const url =
+  'https://www.docuclipper.com/api/v1/protected/document?asyncProcessing=false';
+const formData = new FormData();
+formData.append('document', fs.createReadStream(filePath));
+const headers = {
+  Authorization: `Bearer ${jwtToken}`,
+  ...formData.getHeaders(),
+};
+axios
+  .post(url, formData, { headers })
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+//
 
 /**
  * GET route template
